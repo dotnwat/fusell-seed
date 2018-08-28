a1589617a3295ce3955d3dcc5dd57ebe772dc760
Noah Watkins <noahwatkins@gmail.com>
2018-08-27 21:47:43 -0700

    test: upgrade postgres
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- test/postgres.sh
+++ test/postgres.sh
@@ -2,15 +2,18 @@
 set -e
 set -x
 
-DIR=postgresql-9.5.1
-curl -O https://ftp.postgresql.org/pub/source/v9.5.1/postgresql-9.5.1.tar.gz
-tar xzf postgresql-9.5.1.tar.gz
-pushd $DIR
+VERSION=10.5
+
+curl -O https://ftp.postgresql.org/pub/source/v${VERSION}/postgresql-${VERSION}.tar.gz
+tar xzf postgresql-${VERSION}.tar.gz
+
+DIR=postgresql-${VERSION}
+pushd ${DIR}
 
 ./configure
-make -j10
+make -j$(nproc)
 make check
 
 popd
-rm -rf $DIR
-rm postgresql-9.5.1.tar.gz
+rm -rf ${DIR}
+rm postgresql-${VERSION}.tar.gz
