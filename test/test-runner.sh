2ac7165c8b9db2ba1614ac2f824114404b3fa9c9
Noah Watkins <noahwatkins@gmail.com>
2018-08-29 19:12:26 -0700

    test: add kernel compile test
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- test/test-runner.sh
+++ test/test-runner.sh
@@ -4,18 +4,28 @@ set -x
 
 fs=${1}
 script=${2}
-size=1073741824
+size=1610612736
 dir=$(mktemp -d)
 
 ${fs} -o size=${size} ${dir} &
 pid=$!
 
+function cleanup() {
+  fusermount -u ${dir} || true
+  kill ${pid} || true
+}
+
+trap "cleanup" EXIT
+
+retries=5
 until findmnt -M ${dir} -t fuse.main; do
-  echo "waiting..."
+  retries=$((${retries} - 1))
+  [[ ${retries} -gt 0 ]] || exit 1
+  echo "waiting... ${retries}"
+  sleep 1
 done
 
+# TODO add some stats checks that the fs is empty after cleanup
 pushd ${dir}
 ${script}
 popd
-
-kill ${pid}
